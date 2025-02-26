367d3f965904090fe1ece6eb884e52aaa3404b47
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2025-02-26 15:24:36 +0100

    replace hue naming with condition naming

--- sys_micro_pytools/visualize/visualize.py
+++ sys_micro_pytools/visualize/visualize.py
@@ -5,36 +5,32 @@ from matplotlib import colormaps
 from matplotlib import pyplot as plt
 
 
-def create_palette(plate_layout: pd.DataFrame, hue_vars: Union[Tuple, List], cmap: str ='tab20', 
-                   remove_hue_combo: Union[tuple, list, None] = None, 
-                   hue_start_rgba: Union[tuple, list, None] = None) -> dict:
-        # Get all values of hue_vars
-        hue_var_vals = []
-        for hue_var in hue_vars:
-            if hue_var not in plate_layout.columns:
-                raise ValueError(f'{hue_var} is not a column in the plate layout file')
-            hue_var_vals.append(plate_layout[hue_var].drop_duplicates().values)
+def create_palette(plate_layout: pd.DataFrame, condition_vars: Union[Tuple, List], 
+                   cmap: str ='tab20', conditions2remove: Union[tuple, list, None] = None) -> dict:
+    # Get all values of condition_vars
+    condition_vals = []
+    for condition_var in condition_vars:
+        if condition_var not in plate_layout.columns:
+            raise ValueError(f'{condition_var} is not a column in the plate layout file')
+        condition_vals.append(plate_layout[condition_var].drop_duplicates().values)
 
-        # Make all combinations of all posible hue_var_vals pairs
-        hue_order = list(itertools.product(*hue_var_vals))
+    # Make all combinations of all posible condition_vals pairs
+    condition_order = list(itertools.product(*condition_vals))
 
-        # Remove hue combinations
-        if remove_hue_combo is not None:
-            for hue_combo in remove_hue_combo:
-                if hue_combo in hue_order:
-                    hue_order.remove(hue_combo)
-        
-        # Create palette with remaining hue combinations
+    # Remove condition combinations
+    if conditions2remove is not None:
+        for condition in conditions2remove:
+            if condition in condition_order:
+                condition_order.remove(condition)
+    
+    # Create palette with remaining condition combinations
+    try:
+        cmap = colormaps[cmap]
+    except KeyError:
         try:
-            cmap = colormaps[cmap]
-        except KeyError:
-            try:
-                cmap = plt.cm.get_cmap(f'cet_{cmap}')
-            except ValueError:
-                raise ValueError(f'{cmap} is not a valid colormap')
-
-        colors = [cmap(i) for i in range(len(hue_order))]
-        if hue_start_rgba is not None:
-            colors = (tuple(hue_start_rgba),) + tuple(colors[:-1])
-        palette = {hue: color for hue, color in zip(hue_order, colors)}
-        return palette, hue_order
+            cmap = plt.cm.get_cmap(f'cet_{cmap}')
+        except ValueError:
+            raise ValueError(f'{cmap} is not a valid colormap')
+    colors = [cmap(i) for i in range(len(condition_order))]
+    palette = {condition: color for condition, color in zip(condition_order, colors)}
+    return palette, condition_order
