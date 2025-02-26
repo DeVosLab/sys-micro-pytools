fd476afc2d3ac586dc9e799306d8bc8bc8fac245
Tim Van De Looverbosch <tim.vandelooverbosch@gmail.com>
2025-02-26 14:58:40 +0100

    Fix function exposion

--- sys_micro_pytools/visualize/__init__.py
+++ sys_micro_pytools/visualize/__init__.py
@@ -1,14 +1,15 @@
+from .visualize import create_palette
+
 from .grid_plots import (
-    create_grid_plot,
-    create_palette,
-    get_df_images,
-    link_df2plate_layout
+    create_grid_plot,    
+    get_df_images
 )
 
+from .channel_plots import create_channel_plots
 
 __all__ = [
-    'create_grid_plot',
     'create_palette',
+    'create_channel_plots',
+    'create_grid_plot',
     'get_df_images',
-    'link_df2plate_layout'
 ]
